4296fb95b4c58c5bbd81afd8fbdf117db2c0324c
raquel-raquel-esperanza <145237485+raquel-raquel-esperanza@users.noreply.github.com>
2024-08-22 22:58:08 -0600

    Add files via upload
    Trabajo realizado por alumnos de 2° Desarrollo de Software

---
+++ perturbaciones.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perturbaciones Naturales y Antropicas</title>
+    <link rel="stylesheet" href="styles.css">   
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="navbar">
+                <li><a href="#naturales" >Perturbaciones Naturales</a></li>
+                <li><a href="#antropicas">Perturbaciones Antropicas</a></li>
+                <li><a href="#mapa">Mapa Conceptual</a></li>
+                <li><a href="#recursos adicionales">Recursos Adicionales</a></li>
+                <li><a href="#infografia">Infografía</a></li>
+                <li><a href="#recursos">Recursos</a></li>
+                <li><a href="#bibliografia">Bibliografía</a></li>
+            </ul>
+        </nav>
+        <h1 class="tema">Perturbaciones Naturales y Antropicas</h1>
+    </header>
+    <main>
+        <section id="naturales">
+            <h2><center>Perturbaciones Naturales</center></h2>
+            <p>Las perturbaciones naturales son aquellos eventos o procesos que alteran un ecosistema sin intervención directa del ser humano. Estos eventos pueden variar en escala, desde fenómenos locales como incendios forestales hasta eventos globales como erupciones volcánicas y terremotos.</p>
+                <p><b>Causas:</b>
+                    Las perturbaciones naturales son el resultado de procesos geológicos, climáticos y biológicos. Por ejemplo, los incendios forestales pueden ser causados por rayos, los terremotos por el movimiento de las placas tectónicas y las erupciones volcánicas por la actividad interna de la Tierra.</p>
+                <p><b>Ejemplos:</b>
+                Además de los mencionados, otros ejemplos de perturbaciones naturales incluyen inundaciones, sequías, huracanes, deslizamientos de tierra y cambios en los patrones climáticos a largo plazo.
+                </p>
+                <p><b>Presentacion en la vida diaria:</b>
+                Las perturbaciones naturales son parte integral de la vida en la Tierra y han moldeado los ecosistemas durante millones de años. Sin embargo, sus efectos pueden ser devastadores para las comunidades humanas y los ecosistemas, causando pérdidas de vidas, daños a la infraestructura y alteraciones en los ciclos biogeoquímicos.</p>
+        </section>
+        <section id="antropicas">
+            <h2><center>Perturbaciones Antropicas</center></h2>
+            <p>Las perturbaciones antrópicas son cualquier acción o proceso originado por el ser humano que altera la estructura, función o composición de un ecosistema. Estas alteraciones pueden ser directas, como la deforestación o la contaminación, o indirectas, como el cambio climático inducido por las emisiones de gases de efecto invernadero.</p>
+            <p><b>Ejemplos:</b></p>
+            <p><b>•	Deforestación:</b> La tala indiscriminada de árboles para obtener madera, ampliar áreas agrícolas o urbanizar destruye hábitats y reduce la biodiversidad.</p>
+            <p><b>•	Contaminación:</b> La liberación de sustancias tóxicas en el aire, el agua y el suelo afecta a los organismos vivos y altera los ciclos biogeoquímicos.</p>
+            <p><b>•	Cambio climático:</b> El aumento de las temperaturas globales, los cambios en los patrones de precipitación y el aumento del nivel del mar son consecuencias directas de las emisiones de gases de efecto invernadero.</p>
+            <p><b>•	Introducción de especies invasoras:</b> La introducción accidental o intencional de especies exóticas en nuevos ecosistemas puede causar la extinción de especies nativas y alterar las redes tróficas.</p>
+            <p><b>•	Urbanización:</b> La expansión de las ciudades implica la destrucción de hábitats naturales, la fragmentación de los ecosistemas y la generación de grandes cantidades de residuos.</p>
+            <p><b>•	Sobreexplotación de recursos naturales:</b> La sobreexplotación es de origen humano y se concreta extrayendo individuos de una especie vegetal o animal no humana al punto de poner en peligro su reproducción, o utilizando los ecosistemas a una tasa mayor a la de su regeneración natural.</p>
+        </section>
+        <section id="mapa">
+            <h2><center>Mapa Conceptual</center></h2>
+            <P>Observar como en el mapa se divide mejor el tema:</P>
+            <img src="mapa.png" alt="Mapa Conceptual" style="width:100%; height: 500%;">
+        </section>
+        <section id="recursos adicionales">
+            <h2><center>Recursos Adicionales</center></h2>
+            <p>En el siguiente enlace podras encontrar 3 videos interesantes sobre el mismo tema.</p>
+            <img src="video.png" alt="video" style="width:100%; max-width: 395px;">
+            <img src="simulador.png" alt="simulador" style="width:100%; max-width: 355px;">
+            <img src="presentacion.png" alt="presentacion" style="width:100%; max-width: 380px;">
+            <a href="https://drive.google.com/drive/folders/1J9JOKQRpCUHQLSY2JaR4BmecXNq4icZL?usp=sharing">https://drive.google.com/drive/folders/1J9JOKQRpCUHQLSY2JaR4BmecXNq4icZL?usp=sharing</a>
+        </section>
+        <section id="infografia">
+            <h2><CENTER>Infografía</CENTER></h2>
+            <p>También puedes resumir y concretar tú información, con el siguiente material:</p>
+            <a href="https://view.genially.com/66be83189428e331567c623d/interactive-image-poblacion-y-medio-ambiente">https://view.genially.com/66be83189428e331567c623d/interactive-image-poblacion-y-medio-ambiente</a>
+        </section>
+        <section id="recursos">
+            <h2><CENTER>Recursos</CENTER></h2>
+          <CENTER><TABLE WIDTH="60%" HEIGHT="60" BORDER="5"><CENTER>
+          <TR>
+          <TD><CENTER><b>Inteligencia Artificial</b></TD>
+          <TD><CENTER><b>Aplicaciones y programas</b></TD>
+          </TR>
+          <TR>
+          <TD><CENTER>Chat GPT </TD>
+          <TD><CENTER>Visual Studio Code</TD>
+          </TR>
+          <TR>
+          <TD><CENTER>Gemini AI</TD>
+          <TD><CENTER>Canva </TD>
+          </TR>
+          <TR>
+          <TD><CENTER>Eleven Labs</CENTER></TD>
+          <TD><CENTER>Geneally</CENTER></TD>
+        </TR>
+          <TR>
+          <TD><CENTER></CENTER></TD>
+          <TD><CENTER>Adobe</CENTER></TD>
+        </TR>
+          </CENTER></TABLE>
+        </section>
+        
+        <section id="bibliografia">
+            <h2><center>Bibliografía</center></h2>
+         <center><TABLE WIDTH="60%" HEIGHT="60" BORDER="5"><CENTER>
+          <TR>
+          <TD><CENTER><b>Bibliografía</b></TD>
+          </TR>
+            <TR>
+            <TD><CENTER>https://openai.com/chatgpt/</TD>
+            </TR>
+            <TR>
+            <TD><CENTER>https://code.visualstudio.com/</TD>
+            </TR>
+            <TR>
+            <TD><CENTER>https://gemini.google.com/app</TD>
+            </TR>
+            <TR>
+            <TD><CENTER>https://www.canvsa.com</CENTER></TD>
+            </TR>
+            <TR>
+            <TD><CENTER>https://elevenlabs.io/app/speech-synthesis</CENTER></TD>
+            </TR>
+            <TR>
+            <TD><CENTER>https://geneally.com/es/</CENTER></TD>
+            </TR>
+            <TR>
+            <TD><CENTER>https://www.adobe.com/</CENTER></TD>
+          </TR>
+            </CENTER></TABLE>
+        </section>
+    </main>
+    <footer>Derechos reservados © 2024 Desarrollo de Software INSIBO</footer>
+    
+</body>
+</html>
+
